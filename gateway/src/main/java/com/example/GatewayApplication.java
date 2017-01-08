9bcf5e5aa0fcc63aec7bae48f6ae4bd0d8a6d3aa
Denis O <denis@universal-development.com>
2017-01-08 21:24:33 +0200

    Example of gateway controller

--- gateway/src/main/java/com/example/GatewayApplication.java
+++ gateway/src/main/java/com/example/GatewayApplication.java
@@ -1,24 +1,50 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.stereotype.Controller;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
+
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@LoadBalanced
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+
+}
+
+@RestController
+class GatewayController {
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@GetMapping("/list")
+	public String list() {
+		System.out.println("Loading list");
+		//restTemplate.exchange("http://TOMATO-SERVICE/tomatoes", HttpMethod.GET, null,   );
+		ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://tomato-service/tomatoes", String.class);
+		System.out.println("Response: " + responseEntity.getStatusCode());
+		return responseEntity.getBody() + "";
+	}
 
 }
