bc60691d6a4065c51f269563a2814ed87632de4d
Denis O <denis@universal-development.com>
2017-01-08 20:08:58 +0200

    9000 port for tomato service

--- tomato-service/src/main/java/com/example/TomatoApplication.java
+++ tomato-service/src/main/java/com/example/TomatoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,6 +20,7 @@ public class TomatoApplication {
 	public class TomatoController {
 
 		@GetMapping("/tomatoes")
+		@ResponseBody
 		public List<String> listTomatoes() {
 			return Arrays.asList("Red tomato", "Green tomato");
 		}
