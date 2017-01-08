514074dd85886d7876c045548eb775ee3241e5bc
Denis O <denis@universal-development.com>
2017-01-08 21:01:17 +0200

    Zuul based service redirecting

--- gateway/src/main/java/com/example/GatewayApplication.java
+++ gateway/src/main/java/com/example/GatewayApplication.java
@@ -3,13 +3,22 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-
+@EnableZuulProxy
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
+
+
 }
