02cfc254be34137fa3b19094eadd1302d17e7a4d
Denis O <denis@universal-development.com>
2017-01-08 20:06:13 +0200

    Added tomato service

---
+++ tomato-service/src/main/java/com/example/TomatoApplication.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootApplication
+public class TomatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomatoApplication.class, args);
+	}
+
+	@Controller
+	public class TomatoController {
+
+		@GetMapping("/tomatoes")
+		public List<String> listTomatoes() {
+			return Arrays.asList("Red tomato", "Green tomato");
+		}
+
+	}
+
+}
+
+
