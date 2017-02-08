3fcd3bf087aa7ee9d73cdedd5e6363a61872fee5
Denis <denis@universal-development.com>
2017-02-09 00:04:57 +0200

    Added headers listing in mvc method

--- tomato-service/src/main/java/com/example/TomatoApplication.java
+++ tomato-service/src/main/java/com/example/TomatoApplication.java
@@ -1,13 +1,17 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.List;
 
 @SpringBootApplication
@@ -21,9 +25,19 @@ public class TomatoApplication {
 	@Controller
 	public class TomatoController {
 
+		@Autowired
+		private HttpServletRequest httpRequest;
+
 		@GetMapping("/tomatoes")
 		@ResponseBody
 		public List<String> listTomatoes() {
+			System.out.println("headers : " + httpRequest.getHeaderNames());
+			Enumeration<String> headerNames = httpRequest.getHeaderNames();
+			while(headerNames.hasMoreElements()) {
+				String element = headerNames.nextElement();
+				System.out.println( element +" " +  httpRequest.getHeader(element));
+			}
+
 			return Arrays.asList("Red tomato", "Green tomato");
 		}
 
